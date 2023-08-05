f210c9cc5550677936fc73f0d72f1af18c7c1aad
Rosimeire Rodrigues <emilymoreira9090@gmail.com>
2023-08-05 12:57:29 -0300

    first commit

---
+++ gradleproject1/app/src/main/java/gradleproject1/carro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gradleproject1;
+
+/**
+ *
+ * @author emily
+ */
+public class carro extends automovel {
+    carro (String automovel){
+        super (automovel);
+    }
+            
+}
---
+++ gradleproject1/app/src/main/java/gradleproject1/piloto.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gradleproject1;
+
+/**
+ *
+ * @author emily
+ */
+public class piloto extends pessoa{
+    piloto (String nome){
+        super (nome);
+    }
+}
